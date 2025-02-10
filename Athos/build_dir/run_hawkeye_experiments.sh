c12b8ef59421b931e65793ce62de4ddfdc5974c4
wqruan <wenqqiangruan@gmail.com>
2025-02-10 13:06:21 +0800

    update scripts

--- Athos/build_dir/run_hawkeye_experiments.sh
+++ Athos/build_dir/run_hawkeye_experiments.sh
@@ -5,18 +5,18 @@ nohup ./shufflemodel_SCI_OT.out r=1 port=12342 < ../model_input_weights_fixedpt_
 
 sleep 60s
 
-nohup ./mobilemodel_SCI_OT.out r=1 port=12342 < ../model_input_weights_fixedpt_scale_23.inp > log_mobile_server.txt &
+nohup ./mobilemodel_SCI_OT.out r=1 port=12343 < ../model_input_weights_fixedpt_scale_23.inp > log_mobile_server.txt &
 
-./mobilemodel_SCI_OT.out r=2 port=12342 < ../model_input_weights_fixedpt_scale_23.inp > log_mobile_client.txt
-
-sleep 60s
-
-nohup ./resmodel_SCI_OT.out r=1 port=12342 < ../model_input_weights_fixedpt_scale_23.inp > log_resnet_server.txt &
-
-./resmodel_SCI_OT.out r=2 port=12342 < ../model_input_weights_fixedpt_scale_23.inp > log_resnet_client.txt
+./mobilemodel_SCI_OT.out r=2 port=12343 < ../model_input_weights_fixedpt_scale_23.inp > log_mobile_client.txt
 
 sleep 60s
 
-nohup ./densemodel_SCI_OT.out r=1 port=12342 < ../model_input_weights_fixedpt_scale_23.inp > log_densenet_server.txt &
+nohup ./resmodel_SCI_OT.out r=1 port=12344 < ../model_input_weights_fixedpt_scale_23.inp > log_resnet_server.txt &
 
-./densemodel_SCI_OT.out r=2 port=12342 < ../model_input_weights_fixedpt_scale_23.inp > log_densenet_client.txt
+./resmodel_SCI_OT.out r=2 port=12344 < ../model_input_weights_fixedpt_scale_23.inp > log_resnet_client.txt
+
+sleep 60s
+
+nohup ./densemodel_SCI_OT.out r=1 port=12345 < ../model_input_weights_fixedpt_scale_23.inp > log_densenet_server.txt &
+
+./densemodel_SCI_OT.out r=2 port=12345 < ../model_input_weights_fixedpt_scale_23.inp > log_densenet_client.txt
